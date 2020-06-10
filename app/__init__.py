e73b1316bf79dca898db038bd4d56e28c410111c
Omar Medjaouri <omar.medjaouri@swri.org>
2020-06-10 13:25:44 -0500

    Issue with Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,11 @@ from config import Config
 def create_app(bot_instance):
     #Create flask app and pass bot instance
     app = Flask(__name__)
+
+    #Get config variables from the environment
     app.config.from_object(Config)
+    
+    #Pass instance of bot
     app.config['Bot'] = bot_instance
 
     #Import blueprints
